79c4f173e73d27addcf1ba46952d222e0d25eebb
Xhram <87285044+Xhram@users.noreply.github.com>
2024-06-13 16:09:29 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Wheel from "./componets/Wheel.tsx";
+
+function App() {
+    return (
+        <>
+            
+            <Wheel />
+        </>
+    );
+}
+
+export default App;
